df392c6b14a5777a6c15145922e07ce9211f6bfe
papaiza <papaiza@github.com>
2016-11-16 09:17:03 -0500

    Fixed use of constructors in parser.

--- src/group.py
+++ src/group.py
@@ -7,6 +7,7 @@ class Group:
     def __init__(self, gid, gname):
         gid_exists = (group.gid == gid for group in all_groups)
         if not any(gid_exists):
-            self.uid = gid
+            self.gid = gid
             self.user_name = gname
             all_groups.append(self)
+            self.users = []
